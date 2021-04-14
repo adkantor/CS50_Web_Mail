70f8b1f8aafc7c86c5fbe907ab4f86b6cacb0240
Adam Kantor <kantor.a@usa.net>
2021-04-14 18:47:41 +0200

    Specification #1 - Send Mail     implemented

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+  console.log('DOM content reload')
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+
+  // Submit button
+  document.querySelector('#compose-form').addEventListener('submit', (event) => {
+    submit_email(); 
+    load_mailbox('sent'); 
+    event.preventDefault();
+  });
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -23,11 +30,38 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function submit_email() {
+ 
+  // get data from form
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+
+  // make API POST request
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result); 
+  })
+  // Catch any errors and log them to the console
+  .catch(error => {
+    console.log('Error:', error);
+  }); 
+
 }
